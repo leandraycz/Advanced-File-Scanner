4bb85ad25d11deb43e76bdb3ce9492073d4a94d0
Leandray Cz <59466836+leandraycz@users.noreply.github.com>
2020-01-12 11:38:15 +0100

    Add files via upload

---
+++ Advanced file scanner/ExportList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Advanced_file_scanner
+{
+    class ExportList
+    {
+        public static void Export(List<string> ListToExport, string ExportFilePath)
+        {
+            StreamWriter Writer = new StreamWriter(ExportFilePath);
+            foreach (var item in ListToExport)
+            {
+                Writer.WriteLine(item.ToString());
+            }
+            Writer.Close();
+        }
+    }
+}
